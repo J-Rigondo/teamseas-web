59699c292c1da795f0a78e1607ac7a8f872d3aaa
futuregoing <futuregoing@prismweaver.com>
2022-07-19 19:14:25 +0900

    아이템 카드

---
+++ components/item-card.tsx
@@ -0,0 +1,38 @@
+interface IItemCard {
+  id: number;
+  title: string;
+  content: string;
+  createAt: string;
+  author: IAuthor;
+}
+
+interface IAuthor {
+  id: number;
+  email: string;
+  name: string;
+}
+
+const ItemCard = ({ id, title, content, createAt, author }: IItemCard) => {
+  return (
+    <div className=" shadow-md p-4 rounded-lg">
+      <div className="w-full h-[200px] bg-slate-500" />
+      <div className="p-3">
+        <div className="flex items-center mb-1">
+          <div className="rounded-full w-5 h-5 bg-slate-500 mr-2" />
+          <span>{author.name}</span>
+        </div>
+        <span>{author.email}</span>
+      </div>
+      <div>
+        <h3>
+          <span className="text-lg font-bold">{id}. </span>
+          {title}
+        </h3>
+        <p>{content}</p>
+        <p>{createAt}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ItemCard;
