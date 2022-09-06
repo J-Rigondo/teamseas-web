0436abb3974973fd577e3e13cd195a777621a7e1
futuregoing <futuregoing@prismweaver.com>
2022-09-06 12:15:33 +0900

    sessionStorage에 현재,이전 경로 저장

--- components/auth/user-loader.tsx
+++ components/auth/user-loader.tsx
@@ -3,10 +3,23 @@ import { refreshFunc } from "libs/api/auth";
 import { graphQLClient } from "libs/gql/request";
 import { useRecoilState, useResetRecoilState } from "recoil";
 import { authAtom } from "libs/recoil/auth";
+import { useRouter } from "next/router";
+import Cookies from "universal-cookie";
+
+function storePathValues() {
+  const storage = globalThis?.sessionStorage;
+
+  if (!storage) return;
+
+  const prevPath = storage.getItem("currentPath") as string;
+
+  storage.setItem("prevPath", prevPath);
+  storage.setItem("currentPath", globalThis.location.pathname);
+}
 
 const UserLoader = () => {
   const [user, setUser] = useRecoilState(authAtom);
-  const resetUser = useResetRecoilState(authAtom);
+  const router = useRouter();
 
   useEffect(() => {
     (async () => {
@@ -21,6 +34,15 @@ const UserLoader = () => {
       }
     })();
   }, []);
+
+  useEffect(() => {
+    storePathValues();
+  }, [router.asPath]);
+
+  //url로 접근 시 /posts/[id] 같은 형태
+  // useEffect(() => {
+  //   console.log("router path", router.asPath);
+  // }, [router.asPath]);
 
   return <div></div>;
 };
